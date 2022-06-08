0ebd67221c526486eb59e4c7789cc442e04389a4
i3h <46342327+i3h@users.noreply.github.com>
2022-06-08 23:13:38 +0800

    finish s0001_two_sum

---
+++ src/s0001_two_sum.rs
@@ -0,0 +1,28 @@
+pub struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut m = HashMap::with_capacity(nums.len());
+        for (i, n) in nums.iter().enumerate() {
+            if let Some(j) = m.get(&(target - *n)) {
+                return vec![*j, i as i32];
+            }
+            m.insert(n, i as i32);
+        }
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn two_sum() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![3, 3], 6));
+    }
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cargo test $1 --lib -- --nocapture
