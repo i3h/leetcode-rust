8ccfad9d209ad2234d842bcb7b8c82c90b431b39
i3h <46342327+i3h@users.noreply.github.com>
2022-06-30 18:07:19 +0800

    s0710_random_pick_with_blacklist

--- src/solution/s0710_random_pick_with_blacklist.rs
+++ src/solution/s0710_random_pick_with_blacklist.rs
@@ -1,5 +1,6 @@
-use rand::Rng;
 use std::collections::HashMap;
+
+use rand::Rng;
 
 struct Solution {
     n: i32,
@@ -9,23 +10,49 @@ struct Solution {
 
 impl Solution {
     fn new(n: i32, blacklist: Vec<i32>) -> Self {
-        let map = blacklist
-            .iter()
-            .enumerate()
-            .fold(HashMap::new(), |mut map, (i, &n)| {
-                map.insert(n, i as i32);
-                map
-            });
+        let mut blacklist = blacklist;
+        blacklist.sort();
+        let m = blacklist.len() as i32;
+        let mut map = HashMap::new();
+
+        // println!("n: {}", n);
+        // println!("m: {}", m);
+        // println!("blacklist: {:?}", blacklist);
+
+        let mut map_blacklist = HashMap::new();
+        for i in 0..m {
+            map_blacklist.insert(blacklist[i as usize], i);
+        }
+        // println!("map_blacklist: {:?}", map_blacklist);
+
+        let mut i = 0;
+        let mut j = n - 1;
+        if m != 0 {
+            while blacklist[i as usize] < n - m {
+                while map_blacklist.contains_key(&j) {
+                    j -= 1;
+                }
+                map.insert(blacklist[i as usize], j);
+                i += 1;
+                if i > m - 1 {
+                    break;
+                }
+                j -= 1;
+            }
+        }
+
+        // println!("map: {:?}", map);
 
         Self { n, blacklist, map }
     }
 
     fn pick(&self) -> i32 {
+        let m = self.blacklist.len() as i32;
         let mut rng = rand::thread_rng();
-        let mut i = rng.gen_range(0, self.n);
+        let mut i = rng.gen_range(0, self.n - m);
 
-        while self.map.contains_key(&i) {
-            i = rng.gen_range(0, self.n);
+        if self.map.contains_key(&i) {
+            i = *self.map.get(&i).unwrap();
         }
 
         i
@@ -38,7 +65,19 @@ mod tests {
 
     #[test]
     fn test_710() {
-        let mut s = Solution::new(7, vec![2, 3, 5]);
+        // let mut s = Solution::new(7, vec![2, 3, 5]);
+        // for _ in 0..10 {
+        //     println!("{}", s.pick());
+        // }
+        // let mut s = Solution::new(1, vec![]);
+        // for _ in 0..10 {
+        //     println!("{}", s.pick());
+        // }
+        // let mut s = Solution::new(3, vec![1]);
+        // for _ in 0..10 {
+        //     println!("{}", s.pick());
+        // }
+        let mut s = Solution::new(3, vec![2]);
         for _ in 0..10 {
             println!("{}", s.pick());
         }
