bdde795f47c0942450f1f1c3e4994696960333b7
i3h <46342327+i3h@users.noreply.github.com>
2022-07-05 19:07:54 +0800

    s1267_count_servers_that_communicate

--- src/solution/s1267_count_servers_that_communicate.rs
+++ src/solution/s1267_count_servers_that_communicate.rs
@@ -2,7 +2,42 @@ pub struct Solution {}
 
 impl Solution {
     pub fn count_servers(grid: Vec<Vec<i32>>) -> i32 {
-        0
+        let mut count = 0;
+        let mut sum = 0;
+        let mut row: Vec<i32> = vec![0; grid.len()];
+        let mut col: Vec<i32> = vec![0; grid[0].len()];
+
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == 1 {
+                    row[i] += 1;
+                    col[j] += 1;
+                    sum += 1;
+                }
+            }
+        }
+
+        for i in 0..row.len() {
+            if row[i] == 1 {
+                for j in 0..col.len() {
+                    if grid[i][j] == 1 {
+                        if col[j] == 1 {
+                            // println!("{} {}", i, j);
+                            count += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // println!("sum: {}", sum);
+        // println!("row: {:?}", row);
+        // println!("col: {:?}", col);
+        // println!("count: {}", count);
+        // println!("final: {}", sum - count);
+
+        sum - count
     }
 }
 
@@ -12,6 +47,12 @@ mod tests {
 
     #[test]
     fn test_1267() {
+        // Solution::count_servers(vec![
+        //     vec![1, 1, 0, 0],
+        //     vec![0, 0, 1, 0],
+        //     vec![0, 0, 1, 0],
+        //     vec![0, 0, 0, 1],
+        // ]);
         assert_eq!(0, Solution::count_servers(vec![vec![1, 0], vec![0, 1]]));
         assert_eq!(3, Solution::count_servers(vec![vec![1, 0], vec![1, 1]]));
         assert_eq!(
